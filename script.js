bdb10d8ca9fb1358155463cf3b6f1898dc8b3325
TheWingsGroup <wingsoffuture.in@gmail.com>
2025-04-13 07:29:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,11 @@
-const canvas = document.getElementById("bgCanvas");
-const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let particlesArray = [];
-
-class Particle {
-  constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 8 + 4;
-    this.speedX = Math.random() * 1 - 0.5;
-    this.speedY = Math.random() * 1 - 0.5;
-    this.color = 'rgba(0, 194, 255, 0.6)';
-  }
-
-  update() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-
-    // wrap around screen
-    if (this.x > canvas.width) this.x = 0;
-    if (this.x < 0) this.x = canvas.width;
-    if (this.y > canvas.height) this.y = 0;
-    if (this.y < 0) this.y = canvas.height;
-  }
-
-  draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.shadowBlur = 15;
-    ctx.shadowColor = "#00C2FF";
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.fill();
-  }
-}
-
-function init() {
-  particlesArray = [];
-  for (let i = 0; i < 25; i++) {
-    particlesArray.push(new Particle());
-  }
-}
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  particlesArray.forEach(p => {
-    p.update();
-    p.draw();
+window.addEventListener('scroll', () => {
+  const elements = document.querySelectorAll('.team, .mission, .releases, .contact');
+  elements.forEach(el => {
+    const pos = el.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+    if (pos < windowHeight - 100) {
+      el.style.opacity = 1;
+      el.style.transform = 'translateY(0)';
+    }
   });
-  requestAnimationFrame(animate);
-}
-
-window.addEventListener('resize', () => {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-  init();
 });
-
-init();
-animate();
